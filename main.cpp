4400db9acb8b925af97f9bb32bf4ce2473db6536
Lionel <yunnuol@student.unimelb.edu.au>
2025-11-18 20:26:50 +1100

    set up scene, viewport, camera

--- main.cpp
+++ main.cpp
@@ -1,24 +1,46 @@
 #include <iostream>
 #include "colour.h"
+#include "ray.h"
 #include "vec3.h"
 
+colour ray_colour(const ray& r) {
+    vec3 unit_direction = unit_vector(r.direction());
+    auto a = 0.5*(unit_direction.y() + 1.0);
+    return (1.0-a)*colour(1.0, 1.0, 1.0) + a*colour(0.5, 0.7, 1.0);
+}
+
 int main() {
-    int image_width = 256;
-    int image_height = 256;
 
-    std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
+    auto aspect_ratio = 16.0 / 9.0;
+    int image_width = 400;
 
-    for (int j = 0; j < image_height; ++j) {
-        std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
-        for (int i = 0; i < image_width; ++i) {
-            auto r = double(i) / (image_width - 1);
-            auto g = double(j) / (image_height - 1);
-            auto b = 0.0;
+    int image_height = int(image_width / aspect_ratio);
+    image_height = (image_height < 1) ? 1 : image_height;
 
-            auto pixel_colour = colour(r, g, b);
+    // Camera
+    auto focal_length = 1.0;
+    auto viewport_height = 2.0;
+    auto viewport_width = viewport_height * (double(image_width)/image_height);
+    auto camera_center = point3(0, 0, 0);
+
+    auto viewport_u = vec3(viewport_width, 0, 0);
+    auto viewport_v = vec3(0, -viewport_height, 0);
+
+    auto pixel_delta_u = viewport_u / image_width;
+    auto pixel_delta_v = viewport_v / image_height;
+
+    auto viewport_upper_left = camera_center - vec3(0, 0, focal_length) - viewport_u/2 - viewport_v/2;
+    auto pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
+
+    // Render
+    for (int j = 0; j < image_height; j++) {
+        for (int i = 0; i < image_width; i++) {
+            auto pixel_center = pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
+            auto ray_direction = pixel_center - camera_center;
+            ray r(camera_center, ray_direction);
+
+            colour pixel_colour = ray_colour(r);
             write_colour(std::cout, pixel_colour);
         }
     }
-
-    std::clog << "\rDone!\n";
 }
