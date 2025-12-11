09378706307ec1041beae0762157471f2b37f8a2
Lionel <yunnuol@student.unimelb.edu.au>
2025-12-11 16:38:03 +1100

    implemented anti aliasing

--- colour.h
+++ colour.h
@@ -5,6 +5,7 @@
 #ifndef COLOUR_H
 #define COLOUR_H
 
+#include "interval.h"
 #include "vec3.h"
 
 using colour = vec3;
@@ -14,9 +15,11 @@ void write_colour(std::ostream& out, const colour& c) {
     auto g = c.y();
     auto b = c.z();
 
-    int rbyte = int(255.999 * r);
-    int gbyte = int(255.999 * g);
-    int bbyte = int(255.999 * b);
+    // Translate the [0,1] component values to the byte range [0,255].
+    static const interval intensity(0.000, 0.999);
+    int rbyte = int(256 * intensity.clamp(r));
+    int gbyte = int(256 * intensity.clamp(g));
+    int bbyte = int(256 * intensity.clamp(b));
 
     out << rbyte << ' ' << gbyte << ' ' << bbyte << '\n';
 }
